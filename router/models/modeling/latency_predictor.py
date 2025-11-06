d3006adf317a883c11bda80e0df516c50369d90d
StarryNight-2022 <1345505023@qq.com>
2025-11-06 19:50:36 +0800

    Add (Bert-base-uncased & MLP) output length predictor modeling.

--- router/models/modeling/latency_predictor.py
+++ router/models/modeling/latency_predictor.py
@@ -1,3 +1,17 @@
 # 该文件负责进行Latency的预测，具体有两个技术路线
 # 1.使用小模型Qwen3-0.6B的输出长度来预测大模型的输出长度，这里需要拟合二者之间的关系（如何实现往不同模型的映射？）
 # 2.使用Qwen3-Embeddings-0.6B得到prompt的Embedding，再使用模型拟合这个向量与输出长度的关系。（可以先映射到Qwen3-0.6B的输出长度）
+import torch
+from router.models.modeling.modeling import Bert_MLP
+
+device = torch.device("cuda:1")
+
+predictor = Bert_MLP(bert_dir="/home/ouyk/project/ICDCS/Oracle/model/Bert_Base",
+                     bert_hidden_dim=768,
+                     hidden_size=192,
+                     output_size=16,
+                     device=device)
+
+out = predictor("Plants create [MASK] through a process known as photosynthesis.")
+
+print(out.to("cpu"))
--- router/models/modeling/modeling.py
+++ router/models/modeling/modeling.py
@@ -8,6 +8,8 @@ import pickle
 #---------------------- MLP -----------------------
 import torch
 import torch.nn as nn
+#------------------ Transformers ------------------
+from transformers import BertModel, AutoTokenizer
 
 class KNN():
     def __init__(self, checkpoint:str):
@@ -40,3 +42,46 @@ class MLP(nn.Module):
         x = self.fc3(x)
         # x = self.logsoftmax(x)
         return x
+    
+class Bert_MLP(nn.Module):
+    # "/home/ouyk/project/ICDCS/Oracle/model/Bert_Base"
+    def __init__(self, 
+                 bert_dir:str, 
+                 bert_hidden_dim:int, 
+                 hidden_size:int, 
+                 output_size:int, 
+                 device:torch.device):
+        super(Bert_MLP, self).__init__()
+        self.device = device
+        
+        # 预训练的Bert模型相关内容
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            bert_dir,
+        )
+        # 预训练的Bert模型
+        self.bert = BertModel.from_pretrained(
+            bert_dir,
+            dtype=torch.float16,
+            attn_implementation="sdpa"
+        ).to(self.device)
+        
+        self.dropout = nn.Dropout(0.1)   # 仅在训练过程有效
+        
+        self.fc1 = nn.Linear(bert_hidden_dim, hidden_size, dtype=torch.float16).to(self.device)
+        self.fc2 = nn.Linear(hidden_size, hidden_size, dtype=torch.float16).to(self.device)
+        self.fc3 = nn.Linear(hidden_size, output_size, dtype=torch.float16).to(self.device)
+        self.relu = nn.ReLU().to(self.device)
+
+    def classifier(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+        
+    def forward(self, query:str):
+        input_ids = self.tokenizer(query, return_tensors="pt").to(self.device)
+        bert_outputs = self.bert(**input_ids)
+        pooled_output = bert_outputs[1]
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+        return logits
